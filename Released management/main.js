5b7aec29b681797268f3f70c8645a29e6bcf1ad4
crislopez <30690095+crislopez@users.noreply.github.com>
2017-08-03 09:29:16 -0500

    inventario

---
+++ Released management/main.js
@@ -0,0 +1,32 @@
+new Vue({
+  el: '#ambiente',
+  data: {
+    selected: ''
+  },
+});
+new Vue({
+  el: '#proyecto',
+  data: {
+    selected: ''
+  },
+});
+new Vue({
+  el: '#aplicacion',
+  data: {
+    selected: ''
+  },
+});
+
+new Vue({
+  el: '#version',
+  data: {
+    mensaje: ''
+  },
+});
+
+new Vue({
+  el: '#fecha',
+  data: {
+    mensaje: ''
+  },
+});
